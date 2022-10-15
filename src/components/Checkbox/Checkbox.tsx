9dd845008b3ac4ae58d5207b48b5a874cb347fbe
Marina <marinapq@MacBook-Air-de-Marina.local>
2022-10-15 13:02:01 -0300

    fix checkbox disabled style

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -18,7 +18,7 @@ export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {};
 
 function CheckboxInput(props: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root {...props} className='w-6 h-6 p-[2px] rounded bg-zinc-700 disabled:opacity-50'>
+    <CheckboxPrimitive.Root {...props} className='w-6 h-6 p-[2px] rounded bg-zinc-700 disabled:opacity-60'>
       <CheckboxPrimitive.Indicator asChild>
         <Check weight='bold' className='h-5 w-5 text-indigo-500' />
       </CheckboxPrimitive.Indicator>
